7398833cf6acf838f449c649244705cad09b10d8
Erica Chesley <erchesley@gmail.com>
2020-05-21 16:53:46 -0700

    Trading cards implemented in batch

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,5 +1,56 @@
 "use strict";
 
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
+
+
 class TradingCard extends React.Component {
   render() {
     return (
@@ -12,35 +63,32 @@ class TradingCard extends React.Component {
   }
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
+
+class TradingCardContainer extends React.Component {
+  render() {
+    const tradingCards = [];
+    for (const card of tradingCardData) {
+      tradingCards.push(
+        <TradingCard
+        key= {card.name}
+        name= {card.name}
+        skill= {card.skill}
+        imgUrl= {card.imgUrl}
+        />
+      );
+    }
+
+    return (
+      <div>
+      {tradingCards}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
+  <TradingCardContainer />,
+  document.querySelector('#trading-card-container')
 );
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
+
