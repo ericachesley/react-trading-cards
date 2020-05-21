22be959dba899b45819d089684fa108026634462
Erica Chesley <erchesley@gmail.com>
2020-05-21 16:12:36 -0700

    Steps 1 and 2 complete

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -4,7 +4,14 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        This component needs to be implemented.
+        <p>Nice to have you here!</p>
+        <div>
+            <a href='/cards'>Cards</a>
+            <p></p>
+        </div>
+        <div>
+            <img src='/static/img/balloonicorn.jpg'/>
+        </div>
       </div>
     );
   }
